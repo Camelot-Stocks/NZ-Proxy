f0beba9e67427f148d197b480b81b6c0f8c3e14a
nickzylstra <nick.zylstra@gmail.com>
2020-01-02 19:54:47 -0800

    Fix index linting errors

--- proxy-server/index.js
+++ proxy-server/index.js
@@ -1,5 +1,6 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+
+const app = express();
 const port = 3000;
 const path = require('path');
 const cors = require('cors');
@@ -8,69 +9,69 @@ app.use('/', express.static(path.join(__dirname)));
 app.use(cors());
 
 app.get('/about/getData', (req, res) => {
-    var id = req.params.id || 1;
-    res.redirect(`http://13.52.245.200/about/getData/?id=${id}`);
-})
+  const id = req.params.id || 1;
+  res.redirect(`http://13.52.245.200/about/getData/?id=${id}`);
+});
 
 app.get('/ratings/getData', (req, res) => {
-    var id = req.params.id || 1;
-    res.redirect(`http://54.153.72.27/ratings/getData/?id=${id}`);
-})
+  const id = req.params.id || 1;
+  res.redirect(`http://54.153.72.27/ratings/getData/?id=${id}`);
+});
 
 app.get('/ratings/:photo', (req, res) => {
-    res.redirect(`http://54.153.72.27${req.url}`)
-})
+  res.redirect(`http://54.153.72.27${req.url}`);
+});
 
 
 app.get('/earnings/getData', (req, res) => {
-    res.redirect('http://54.67.103.66/earnings/getData');
-})
+  res.redirect('http://54.67.103.66/earnings/getData');
+});
 
 app.get('/news/getData', (req, res) => {
-    res.redirect('http://54.193.67.89/news/getData');
-})
+  res.redirect('http://54.193.67.89/news/getData');
+});
 
 
 app.get('/tradestock/api', (req, res) => {
-    var id = req.params.id || 1;
-    res.redirect(`http://34.214.68.82/tradestock/api/?id=${id}`);
+  const id = req.params.id || 1;
+  res.redirect(`http://34.214.68.82/tradestock/api/?id=${id}`);
 });
 
 app.get('/questionMark.png', (req, res) => {
-    res.redirect('http://34.214.68.82/questionMark.png')
-})
+  res.redirect('http://34.214.68.82/questionMark.png');
+});
 
 app.get('/exclamation.png', (req, res) => {
-    res.redirect('http://34.214.68.82/exclamation.png')
-})
+  res.redirect('http://34.214.68.82/exclamation.png');
+});
 
 app.get('/exclamation-button.png', (req, res) => {
-    res.redirect('http://34.214.68.82/exclamation-button.png')
-})
+  res.redirect('http://34.214.68.82/exclamation-button.png');
+});
 
 app.get('/arrows.png', (req, res) => {
-    res.redirect('http://34.214.68.82/arrows.png')
-})
+  res.redirect('http://34.214.68.82/arrows.png');
+});
 
 app.get('/arrows_black.png', (req, res) => {
-    res.redirect('http://34.214.68.82/arrows_black.png')
-})
+  res.redirect('http://34.214.68.82/arrows_black.png');
+});
 
 
 app.get('/graph/getStocks', (req, res) => {
-    res.redirect('http://54.153.91.76/graph/getStocks');
-})
+  res.redirect('http://54.153.91.76/graph/getStocks');
+});
 
 app.get('/graph/img/:photo', (req, res) => {
-    res.redirect(`http://54.153.91.76/graph/img/${path.basename(req.url)}`)
-})
+  res.redirect(`http://54.153.91.76/graph/img/${path.basename(req.url)}`);
+});
 
 app.get('/navbar/img/:photo', (req, res) => {
-    res.redirect(`http://54.219.176.99/navbar/img/${path.basename(req.url)}`)
-})
+  res.redirect(`http://54.219.176.99/navbar/img/${path.basename(req.url)}`);
+});
 
 app.post('/updateLineColors', (req, res) => {
-    res.end();
-})
+  res.end();
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
